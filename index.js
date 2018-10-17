f3340cc663744f24ec54dc99f1af7d8a7873f3b0
Mary Palumbo <mpalumbo@conductor.com>
2018-10-17 00:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code in this file!
 let currentUser = 'Mary'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
-let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase()
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
+
 
